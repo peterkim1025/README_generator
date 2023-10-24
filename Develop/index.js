5f856054bb6a61bc100ab0a2621e2534dea7a916
peter kim <peterkim1025@gmail.com>
2023-10-24 19:02:02 -0400

    i think i finished

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'userName',
+      message: 'What is your GitHub name?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'What is the name of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a short description of your project:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license does your project have?',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+    },
+    {
+      type: 'input',
+      name: 'dependencies',
+      message: 'What command should be run to install dependencies?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What command should be run to run tests?',
+    },
+    {
+      type: 'input',
+      name: 'repoKnowledge',
+      message: 'What does the user need to know about using this repo?',
+    },
+    {
+      type: 'input',
+      name: 'repoContribution',
+      message: 'What does the user need to know about contributing to this repo?',
+    },
+  ])
+  .then((answers) => {
+    const readmeContent = generateMarkdown(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    fs.writeFile('README.md', readmeContent, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Successfully created README.md!');
+      }
+    });
+  });
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,70 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "APACHE 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GPL 3.0":
+      return "[![License: GPL 3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://opensource.org/licenses/GPL-3.0)";
+    case "BSD 3":
+      return "[![License: BSD 3](https://img.shields.io/badge/License-BSD%203-blue.svg)](https://opensource.org/licenses/BSD-3)";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseBadge = renderLicenseBadge(data.license);
 
+  return `# ${data.projectName}
+
+${licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.dependencies}
+\`\`\`
+
+## Usage
+
+${data.repoKnowledge}
+
+## License
+
+This project is licensed under the ${data.license} license. See the license section for more information.
+
+## Contribution
+
+${data.repoContribution}
+
+## Tests
+
+To run tests, please run the following command:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, please contact me directly at ${data.email}. You can also find more of my work at [${data.userName}](https://github.com/${data.userName}).
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown };
